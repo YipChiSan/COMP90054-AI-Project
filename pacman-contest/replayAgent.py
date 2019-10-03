63d38891675e14202cb3f92cfa4f3c82f330adb9
cn-wx <xuwang2@student.unimelb.edu.au>
2019-10-03 20:09:53 +1000

    Add state features

--- pacman-contest/replayAgent.py
+++ pacman-contest/replayAgent.py
@@ -11,8 +11,10 @@ class RecordAgent(CaptureAgent):
     self.action = [action for i, action in enumerate(self.agentActions) if self.index == action[0]]
     self.ourMiddleLine = self.getOurMiddleLine(gameState)
     self.enemyMiddleLine = self.getEnemyMiddleLine(gameState)
-    self.enemyCapsules = gameState.getRedCapsules() if self.index & 1 else gameState.getBlueCapsules()
-    self.ourCapsules =  gameState.getBlueCapsules() if self.index & 1 else gameState.getRedCapsules()
+    self.initEnemyFood = gameState.getRedFood() if self.index & 1 else gameState.getBlueFood()
+    self.initOurFood =  gameState.getBlueFood() if self.index & 1 else gameState.getRedFood()
+    self.initEnemyCapsules = gameState.getRedCapsules() if self.index & 1 else gameState.getBlueCapsules()
+    self.initOurCapsules =  gameState.getBlueCapsules() if self.index & 1 else gameState.getRedCapsules()
 
     self.recordData = []
     self.preAction = None
@@ -53,6 +55,18 @@ class RecordAgent(CaptureAgent):
       new_index = new_index-4
     return new_index
 
+  def getEnemyNearestFood(self, getMazeDistance, ourFood, firstEnemyCurPosition, secondEnemyCurPosition):
+      minDistance = 99999
+      minFood = None
+      for i, food in enumerate(ourFood.asList()):
+        dis1 = getMazeDistance(firstEnemyCurPosition, food) if firstEnemyCurPosition else 99999
+        dis2 = getMazeDistance(secondEnemyCurPosition, food) if secondEnemyCurPosition else 99999
+        dis = min(dis1, dis2)
+        if dis < minDistance:
+          minDistance = dis
+          minFood = food
+      return (minDistance, minFood)
+
   def chooseAction(self, gameState):
 
     # ['agentDistances', 'blueTeam', 'data', 'deepCopy', 'generateSuccessor', 'getAgentDistances', 'getAgentPosition',
@@ -81,10 +95,18 @@ class RecordAgent(CaptureAgent):
     firstEnemyCurPosition = gameState.getAgentPosition(firstEnemyIndex)
     secondEnemyCurPosition = gameState.getAgentPosition(secondEnemyIndex)
 
+    enemyFood = gameState.getRedFood() if self.index & 1 else gameState.getBlueFood()
+    ourFood =  gameState.getBlueFood() if self.index & 1 else gameState.getRedFood()
+    enemyCapsules = gameState.getRedCapsules() if self.index & 1 else gameState.getBlueCapsules()
+    ourCapsules =  gameState.getBlueCapsules() if self.index & 1 else gameState.getRedCapsules()
+
 
     ##################################################################
     # 分数
     score = gameState.data.score
+
+    # 剩余时间
+    timeleft = gameState.data.timeleft
 
     # 是不是红方
     isRed = gameState.isOnRedTeam(myIndex)
@@ -146,12 +168,34 @@ class RecordAgent(CaptureAgent):
     distanceToEnemyMiddleLine = min(map(lambda x: self.getMazeDistance(myCurPosition, x), self.enemyMiddleLine))
 
     # 自己距离敌方最近capsule的距离
-    distanceToEnemyCapsule = min(map(lambda x: self.getMazeDistance(myCurPosition, x), self.enemyCapsules)) if self.enemyCapsules else 99999
+    distanceToEnemyCapsule = min(map(lambda x: self.getMazeDistance(myCurPosition, x), enemyCapsules)) if enemyCapsules else 99999
     # 第一个敌人距离我方capsule的最近距离
-    distanceFromFirstEnemyToOurCapsule = min(map(lambda x: self.getMazeDistance(firstEnemyCurPosition, x), self.ourCapsules)) if firstEnemyCurPosition and self.ourCapsules else 99999
+    distanceFromFirstEnemyToOurCapsule = min(map(lambda x: self.getMazeDistance(firstEnemyCurPosition, x), ourCapsules)) if firstEnemyCurPosition and ourCapsules else 99999
     # 第二个敌人距离我方capsule的最近距离
-    distanceFromSecondEnemyToOurCapsule = min(map(lambda x: self.getMazeDistance(secondEnemyCurPosition, x), self.ourCapsules)) if secondEnemyCurPosition and self.ourCapsules else 99999
+    distanceFromSecondEnemyToOurCapsule = min(map(lambda x: self.getMazeDistance(secondEnemyCurPosition, x), ourCapsules)) if secondEnemyCurPosition and ourCapsules else 99999
 
+    # 我方剩余豆子个数
+    ourRestFoodCount = ourFood.count()
+    # 敌方剩余豆子个数
+    enemyRestFoodCount = enemyFood.count()
+    # 我方豆子总数
+    ourFoodTotal = self.initOurFood.count()
+    # 敌方豆子总数
+    enemyFoodTotal = self.initEnemyFood.count()
+
+    # 自己身上吃了几个豆子
+    foodCarrying = gameState.data.agentStates[myIndex].numCarrying
+
+    # 敌人无敌时间
+    myScaredTimer = gameState.data.agentStates[myIndex].scaredTimer
+    # 自己无敌时间
+    myInvincibleTimer = min(gameState.data.agentStates[firstEnemyIndex].scaredTimer, gameState.data.agentStates[secondEnemyIndex].scaredTimer)
+
+    __enemyNearestFoodAndDistance = self.getEnemyNearestFood(self.getMazeDistance, ourFood, firstEnemyCurPosition, secondEnemyCurPosition)
+    # 敌人到我方豆子最近到豆子(如果可见到话), 坐标从左上角开始
+    distanceFromFirstEnemyToOurFood = __enemyNearestFoodAndDistance[0]
+    # 我方到离敌人最近豆子到距离
+    distanceToEnemyNearestFood = min(self.getMazeDistance(myCurPosition, __enemyNearestFoodAndDistance[1]), self.getMazeDistance(teammateCurPosition, __enemyNearestFoodAndDistance[1])) if __enemyNearestFoodAndDistance[1] else 99999
 
     print('------')
     ##################################################################
