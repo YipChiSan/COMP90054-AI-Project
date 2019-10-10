2dee505d6fbfe79d8f8ed37550ec19a895019e1a
cn-wx <xuwang2@student.unimelb.edu.au>
2019-10-11 00:30:41 +1100

    Add scripts for windows

--- pacman-contest/submit.bat
+++ pacman-contest/submit.bat
@@ -1,7 +1,14 @@
-#!/usr/bin/env bash
+@ECHO OFF
+
+ECHO Deleting old tag ...
 git push --delete origin submission-contest
 git tag --delete submission-contest
+
+ECHO Re-tag ...
 git tag submission-contest
+
+ECHO Push to remote...
 git tag -l
 git push origin --tags
 
+PAUSE
